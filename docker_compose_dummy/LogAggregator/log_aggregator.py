3c1b201adb832e3d926d892f85423e1e0a1dd33f
nishi62285 <dhurinishikant@gmail.com>
2020-09-03 03:02:11 +0530

    file path is checked before reading and jenkins removed from yaml

--- docker_compose_dummy/LogAggregator/log_aggregator.py
+++ docker_compose_dummy/LogAggregator/log_aggregator.py
@@ -6,7 +6,7 @@ folder = os.path.dirname(os.path.abspath(__file__))
 folder = folder + '/logs'
 #folder = os.path.abspath(__file__)
 #folder = folder + '\logs'
-
+import os.path
 
 
 def log_down():
@@ -18,34 +18,35 @@ def log_down():
     data=[]
     my_file = os.path.join(folder, 'down.log')
     while True:
-        with open(my_file, 'rb') as f:
-            f.seek(-2, os.SEEK_END)
-            while f.read(1) != b'\n':
-                f.seek(-2, os.SEEK_CUR)
-            lst=f.readline().decode()
-            if lst_line.replace('\n','').replace('\r','')==lst.replace('\n','').replace('\r',''):
-                print('no new update...')
-                time.sleep(10)
-                continue
-        f.close()
-        with open(my_file, 'r') as f1:
+        if os.path.isfile(my_file):
+            with open(my_file, 'rb') as f:
+                f.seek(-2, os.SEEK_END)
+                while f.read(1) != b'\n':
+                    f.seek(-2, os.SEEK_CUR)
+                lst=f.readline().decode()
+                if lst_line.replace('\n','').replace('\r','')==lst.replace('\n','').replace('\r',''):
+                    print('no new update...')
+                    time.sleep(10)
+                    continue
+            f.close()
+            with open(my_file, 'r') as f1:
 
-            for (i, line) in enumerate(f1):
-                if (index >0  and i > index) or index ==0:
-                    print(line)
-                    obj = {}
-                    obj['id'] = i
-                    obj['log'] = line
-                    data.append(obj)
+                for (i, line) in enumerate(f1):
+                    if (index >0  and i > index) or index ==0:
+                        print(line)
+                        obj = {}
+                        obj['id'] = i
+                        obj['log'] = line
+                        data.append(obj)
 
-            index,lst_line =i, line
+                index,lst_line =i, line
 
-            # dictToSend = {'status': os.environ.get('src_system')}
-            # requests.post('http://' + str(os.environ.get('tracer_ip')) + ':' + str(5000) + '/soc', json=dictToSend)
+                # dictToSend = {'status': os.environ.get('src_system')}
+                # requests.post('http://' + str(os.environ.get('tracer_ip')) + ':' + str(5000) + '/soc', json=dictToSend)
 
-        f.close()
-        mongo["down"].insert_many(data)
-        data=[]
+            f.close()
+            mongo["down"].insert_many(data)
+            data=[]
 
 def log_up():
     mongo = 'mongodb://' + os.environ['mongo_addr'] + ':27017/'
@@ -56,35 +57,36 @@ def log_up():
     data=[]
     my_file = os.path.join(folder, 'up.log')
     while True:
-        with open(my_file, 'rb') as f:
-            f.seek(-2, os.SEEK_END)
-            while f.read(1) != b'\n':
-                f.seek(-2, os.SEEK_CUR)
-            lst=f.readline().decode()
-            if lst_line.replace('\n','').replace('\r','')==lst.replace('\n','').replace('\r',''):
-                print('no new update...')
-                time.sleep(10)
-                continue
-        f.close()
-        with open(my_file, 'r') as f1:
+        if os.path.isfile(my_file):
+            with open(my_file, 'rb') as f:
+                f.seek(-2, os.SEEK_END)
+                while f.read(1) != b'\n':
+                    f.seek(-2, os.SEEK_CUR)
+                lst=f.readline().decode()
+                if lst_line.replace('\n','').replace('\r','')==lst.replace('\n','').replace('\r',''):
+                    print('no new update...')
+                    time.sleep(10)
+                    continue
+            f.close()
+            with open(my_file, 'r') as f1:
 
-            for (i, line) in enumerate(f1):
-                if (index >0  and i > index) or index ==0:
-                    print(line)
-                    #data.append('{' + line + '}')
-                    obj = {}
-                    obj['id'] = i
-                    obj['log'] = line
-                    data.append(obj)
+                for (i, line) in enumerate(f1):
+                    if (index >0  and i > index) or index ==0:
+                        print(line)
+                        #data.append('{' + line + '}')
+                        obj = {}
+                        obj['id'] = i
+                        obj['log'] = line
+                        data.append(obj)
 
-            index,lst_line =i, line
+                index,lst_line =i, line
 
-            # dictToSend = {'status': os.environ.get('src_system')}
-            # requests.post('http://' + str(os.environ.get('tracer_ip')) + ':' + str(5000) + '/soc', json=dictToSend)
+                # dictToSend = {'status': os.environ.get('src_system')}
+                # requests.post('http://' + str(os.environ.get('tracer_ip')) + ':' + str(5000) + '/soc', json=dictToSend)
 
-        f.close()
-        mongo['up'].insert_many(data)
-        data = []
+            f.close()
+            mongo['up'].insert_many(data)
+            data = []
 
 def log_gdc():
     mongo = 'mongodb://' + os.environ['mongo_addr'] + ':27017/'
@@ -95,33 +97,34 @@ def log_gdc():
     data=[]
     my_file = os.path.join(folder, 'gcd.log')
     while True:
-        with open(my_file, 'rb') as f:
-            f.seek(-2, os.SEEK_END)
-            while f.read(1) != b'\n':
-                f.seek(-2, os.SEEK_CUR)
-            lst=f.readline().decode()
-            if lst_line.replace('\n','').replace('\r','')==lst.replace('\n','').replace('\r',''):
-                print('no new update...')
-                time.sleep(10)
-                continue
-        f.close()
-        with open(my_file, 'r') as f1:
+        if os.path.isfile(my_file):
+            with open(my_file, 'rb') as f:
+                f.seek(-2, os.SEEK_END)
+                while f.read(1) != b'\n':
+                    f.seek(-2, os.SEEK_CUR)
+                lst=f.readline().decode()
+                if lst_line.replace('\n','').replace('\r','')==lst.replace('\n','').replace('\r',''):
+                    print('no new update...')
+                    time.sleep(10)
+                    continue
+            f.close()
+            with open(my_file, 'r') as f1:
 
-            for (i, line) in enumerate(f1):
-                if (index >0  and i > index) or index ==0:
-                    print(line)
-                    obj={}
-                    obj['id']=i
-                    obj['log']=line
-                    data.append(obj)
-            index,lst_line =i, line
+                for (i, line) in enumerate(f1):
+                    if (index >0  and i > index) or index ==0:
+                        print(line)
+                        obj={}
+                        obj['id']=i
+                        obj['log']=line
+                        data.append(obj)
+                index,lst_line =i, line
 
-            # dictToSend = {'status': os.environ.get('src_system')}
-            # requests.post('http://' + str(os.environ.get('tracer_ip')) + ':' + str(5000) + '/soc', json=dictToSend)
+                # dictToSend = {'status': os.environ.get('src_system')}
+                # requests.post('http://' + str(os.environ.get('tracer_ip')) + ':' + str(5000) + '/soc', json=dictToSend)
 
-        f.close()
-        mongo['gcd'].insert_many(data)
-        data=[]
+            f.close()
+            mongo['gcd'].insert_many(data)
+            data=[]
 
 if __name__ == '__main__':
     #os.environ['mongo_addr'] = '192.168.99.100'
